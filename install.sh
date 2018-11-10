86f72cbdda8f088bc7a5fc9937a89f6169b6104e
StefanSchmelz <stefan_schmelz@gmx.net>
2018-11-10 16:08:20 +0100

    Edited install.sh to be a crude, but working setup to clone all repositorys currently needed and build everything that is working now.

--- install.sh
+++ install.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+echo "Welcome to teamwork-\"Chatbot\" setup assistant."
+
+echo "Cloning repositorys"
+if [ -d "$telegramm-client" ]; then
+	echo "Skipping repository \"telegram-client\""
+else
+	git clone https://github.com/carpe-noctem-cassel/telegram-client.git
+fi
+if [ -d "$tgbot-cpp" ]; then
+	echo "Skipping repository \"tgbot-cpp\""
+else
+	git clone https://github.com/reo7sp/tgbot-cpp.git
+fi
+
+depends="g++ make binutils cmake libssl-dev libboost-system-dev"
+
+echo "Installing neccesary dependencies"
+sudo apt install $depends
+
+echo "Building Telegram api"
+(cd ~/teamwork-ws/tgbot-cpp ; cmake . ; make ; sudo make install)
