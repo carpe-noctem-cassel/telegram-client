860e544fbacc387c0c37785b19027fac1aa4ef6b
Stefan Schmelz <stefan_schmelz@gmx.net>
2018-11-12 12:23:35 +0100

    expanded bot skeleton.

--- src/client.cpp
+++ src/client.cpp
@@ -2,19 +2,17 @@
 #include <string>
 #include <tgbot/tgbot.h>
 
+void reply(TgBot::Bot &bot, TgBot::Message::Ptr message, std::string reply);
+
 int main(int argc, char* argv[])
 {
 	std::string token = "603132548:AAH9RAl4fTiwi2-uxH-YJnzJlEqzC9rgjG8";
 
-	TgBot::TgBot bot(token);
-	auto events = bot.getEvents();
-	events.onCommand("start", [bot&](TgBot::Message::Ptr message){
+	TgBot::Bot bot(token);
+	bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message){
 		bot.getApi().sendMessage(message->chat->id, "Hi!");
 	});
-	events.onNonCommandMessage([bot&](TgBot::Message::Ptr message){
-		bot.getApi().sendMessage(message->chat->id, "Yes Master!")
-		std::cout << "User message detected: " << message->text << '\n';
-	});
+	bot.getEvents().onNonCommandMessage([&bot](TgBot::Message::Ptr message){reply(bot, message, "Yes Master!")});
 
 
 	try {
@@ -25,8 +23,14 @@ int main(int argc, char* argv[])
             longPoll.start();
         }
     } catch (TgBot::TgException& e) {
-        std::cout << "error: " << e.what() <<< '\n';
+        std::cout << "error: " << e.what() << '\n';
     }
 
 	return 0;
+}
+
+void reply(TgBot::Bot &bot, TgBot::Message::Ptr message, std::string reply)
+{
+	bot.getApi().sendMessage(message->chat->id, "Yes Master!")
+	std::cout << "Replying: " << message->text << '\n';
 }
