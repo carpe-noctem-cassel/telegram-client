eaaada8adfc9312885363b1b6a6769e8e70e48eb
StefanSchmelz <sschmelz64@gmail.com>
2018-12-10 08:27:34 +0100

    added loading api key from file

--- src/TelegramBridge.cpp
+++ src/TelegramBridge.cpp
@@ -4,6 +4,9 @@
 #include "Message.h"
 #include <fstream>
 
+#include <unistd.h>
+#include <sys/types.h>
+
 int main(int argc, char *argv[])
 {
     // Only for testing purposes
@@ -15,9 +18,11 @@ int main(int argc, char *argv[])
     ::capnp::MallocMessageBuilder msgBuilder;
     m.toCapnp(msgBuilder);
 
-    std::ifstream input("telegram.key", std::ifstream::in);
+    std::string path = std::getenv("HOME");
+
+    std::ifstream input(path + "/teamwork-ws/src/telegram-client/telegram.key", std::ifstream::in);
     std::string key;
-    input >> key
+    input >> key;
 
 	Robot r(key, "Pinky");
 	r.setupTelegram();
